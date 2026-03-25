416081ef420c9aa872e4392dc98c71b5624a3fe4
Bartholomew-Fishflinger-3rd <Gmancuber2009@gmail.com>
2026-03-25 12:39:48 -0400

    added a menu system

--- absnormal/game.js
+++ absnormal/game.js
@@ -9,29 +9,7 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 //Game State
-let gameState = "title";  //"title" or "playing"
-let currentRoom = 'lab';
-
-//Load Character Sprites
-const absWalk = {
-    up: new Image(),
-    down: new Image(),
-    left: new Image(),
-    right: new Image()
-};
-
-let absDirection = "down";
-let absX = 368;
-let absY = 268;
-let currentBackgroundImage = null;
-
-let keys = {};
-
-const frameWidth = 96;
-const frameHeight = 96;
-const framePerAnimation = 4;
-
-let frameIndex = 0;
+let gameState = "title";  //"title" or   
 
 window.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
 window.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
